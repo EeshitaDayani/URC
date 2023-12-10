d5f77f262084bffdb9056d02b7648f80ae76a550
Eeshita Dayani <eeshitadayani@gmail.com>
2023-12-10 03:01:22 -0500

    Added bullets

--- src/components/IntroductionSection.tsx
+++ src/components/IntroductionSection.tsx
@@ -9,9 +9,9 @@ const IntroductionSection: React.FC = () => {
         <h2 className={styles.title}>About Us</h2>
         <p className={styles.paragraph}>Welcome to the UWaterloo Undergrad Research Club, <br />where curiosity meets opportunity. Join us to:</p>
         <ul className={styles.list}>
-          <li className={styles.listItem}>Engage in exciting research projects</li>
-          <li className={styles.listItem}>Connect with like-minded peers and mentors</li>
-          <li className={styles.listItem}>Explore your academic and professional interests</li>
+          <li className={styles.listItem}> <span>&#8226;</span> Engage in exciting research projects</li>
+          <li className={styles.listItem}> <span>&#8226;</span> Connect with like-minded peers and mentors</li>
+          <li className={styles.listItem}> <span>&#8226;</span> Explore your academic and professional interests</li>
         </ul>
       </div>
     </div>
