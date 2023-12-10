39a5df847e074d163b0b3559e29d25d260de7b36
Eeshita Dayani <eeshitadayani@gmail.com>
2023-12-10 02:57:04 -0500

    Added styling for Intro section

--- src/components/IntroductionSection.tsx
+++ src/components/IntroductionSection.tsx
@@ -1,18 +1,19 @@
-// components/IntroductionSection.tsx
 import React from 'react';
 import styles from '../styles/IntroductionSection.module.css';
 
 const IntroductionSection: React.FC = () => {
   return (
     <div className={styles.introductionSection}>
-      <h2>About Us</h2>
-      <p>Welcome to the UWaterloo Undergrad Research Club, where curiosity meets opportunity. Our mission is to...</p>
-      <p>Join us to:</p>
-      <ul>
-        <li>Engage in exciting research projects</li>
-        <li>Connect with like-minded peers and mentors</li>
-        <li>Explore your academic and professional interests</li>
-      </ul>
+      <img src='./glass.svg' />
+      <div>
+        <h2 className={styles.title}>About Us</h2>
+        <p className={styles.paragraph}>Welcome to the UWaterloo Undergrad Research Club, <br />where curiosity meets opportunity. Join us to:</p>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Engage in exciting research projects</li>
+          <li className={styles.listItem}>Connect with like-minded peers and mentors</li>
+          <li className={styles.listItem}>Explore your academic and professional interests</li>
+        </ul>
+      </div>
     </div>
   );
 };
