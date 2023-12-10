01df3afde8eadc1ad9e38f76bb0fcab37570c3f8
Eeshita Dayani <eeshitadayani@gmail.com>
2023-12-10 02:11:03 -0500

    Made UI changes for HeroSection component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,13 +5,20 @@ import styles from '../styles/HeroSection.module.css'; // Import your CSS module
 const HeroSection: React.FC = () => {
   return (
     <div className={styles.heroSection}>
-      <img src="/twistedLines.svg" className={styles.waveSvg} alt="Twisted Lines" />
-      <img src="/twistedLines.svg" className={styles.waveSvg2} alt="Twisted Lines" />
       <div className={styles.text}>
-        <h1 style={{ fontSize: '3rem', marginBottom: '5rem'}}>Welcome to the Undergraduate Research Club <br /> at University of Waterloo</h1>
-        <p style={{ fontSize: '2.5rem', marginBottom: '3rem'}}>Empowering Students Through Research Opportunities</p>
-        <p>Get Started</p>
+        <h1 style={{ fontSize: '3rem', marginBottom: '5rem'}}>
+            Welcome to the <span style={{ color: '#FFDB58' }}>Undergraduate Research Club</span> <br />
+            at University of Waterloo
+        </h1>
+        <p style={{ fontSize: '2.5rem', marginBottom: '5rem'}}>Empowering Students Through Research Opportunities</p>
+        <p style={{ fontSize: '1.5rem', borderBottom: '2px solid #FFDB58', display: 'inline-block' }}>
+            Let's Get Started
+        </p>
       </div>
+      <svg className={styles.svg} width="400px" height="400px" xmlns="http://www.w3.org/2000/svg">
+        <rect x="10%" y="10%" width="70%" height="70%" fill="none" stroke="#FFDB58" stroke-width="4"/>
+        <rect x="17%" y="5%" width="70%" height="70%" fill="none" stroke="#FFDB58" stroke-width="4" transform="rotate(15 50 50)"/>
+      </svg>
     </div>
   );
 };
