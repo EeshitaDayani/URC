e22c41c88257ebb55eb867cd2b6811e361fcc901
Eeshita Dayani <eeshitadayani@gmail.com>
2023-12-12 00:51:45 -0500

    Updated GetInvolved UI

--- src/components/GetInvolvedSection.tsx
+++ src/components/GetInvolvedSection.tsx
@@ -5,22 +5,16 @@ import styles from '../styles/GetInvolvedSection.module.css';
 const GetInvolvedSection: React.FC = () => {
   return (
     <div className={styles.getInvolvedSection}>
-      <div className={styles.getInvolvedLeftSection}>
-        <h2 className={styles.title}>Get Involved</h2>
-        <p className={styles.paragraph}>Ready to dive into the world of research? <br />Here's how you can get involved:</p>
-        <ul className={styles.list}>
-          <li className={styles.listItem}> <span>&#8226;</span> Join as a member to access exclusive opportunities</li>
-          <li className={styles.listItem}> <span>&#8226;</span> Check out our upcoming events and meetings</li>
-          <li className={styles.listItem}> <span>&#8226;</span> Contact us for more information</li>
-        </ul>
-        <div className={styles.icons}>
-          {/* Placeholder icons go here */}
-          <img src='./instagram.svg' />
-          <img src='./linktree.svg' />
-        </div>
+      <h2 className={styles.title}>Get Involved</h2>
+      <p className={styles.paragraph}>Ready to dive into the world of research? <br />Here's how you can get involved.</p>
+      <div className={styles.buttons}>
+        <button className={styles.button}>Become a member</button>
+        <button className={styles.button}>Contact us</button>
       </div>
-      <div className={styles.getInvolvedRightSection}>
-        <img src='./social.svg' />
+      <div className={styles.icons}>
+        {/* Placeholder icons go here */}
+        <img src='./instagram.svg' alt="Instagram Icon" />
+        <img src='./linktree.svg' alt="Linktree Icon" />
       </div>
     </div>
   );
