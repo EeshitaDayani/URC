80272130c9f75e05919c5caf54e629174df7505e
Eeshita Dayani <eeshitadayani@gmail.com>
2023-12-12 01:04:19 -0500

    UI fixes to GetInvolved Section

--- src/components/GetInvolvedSection.tsx
+++ src/components/GetInvolvedSection.tsx
@@ -5,17 +5,19 @@ import styles from '../styles/GetInvolvedSection.module.css';
 const GetInvolvedSection: React.FC = () => {
   return (
     <div className={styles.getInvolvedSection}>
-      <h2 className={styles.title}>Get Involved</h2>
-      <p className={styles.paragraph}>Ready to dive into the world of research? <br />Here's how you can get involved.</p>
-      <div className={styles.buttons}>
-        <button className={styles.button}>Become a member</button>
-        <button className={styles.button}>Contact us</button>
+      <div className={styles.content}>
+        <h2 className={styles.title}>Get Involved</h2>
+        <p className={styles.paragraph}>Ready to dive into the world of research? <br /><br />Here's how you can get involved.</p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Become a member</button>
+          <button className={styles.button}>Contact us</button>
+        </div>
+        <div className={styles.icons}>
+          <img src='./instagram.svg' alt="Instagram Icon" />
+          <img src='./linktree.svg' alt="Linktree Icon" />
+        </div>
       </div>
-      <div className={styles.icons}>
-        {/* Placeholder icons go here */}
-        <img src='./instagram.svg' alt="Instagram Icon" />
-        <img src='./linktree.svg' alt="Linktree Icon" />
-      </div>
+      <img src='./social.svg' alt="Social Icon" className={styles.socialIcon} />
     </div>
   );
 };
