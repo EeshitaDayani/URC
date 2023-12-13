6e2851270eba4b70279c0565a2e5e4e7db3125da
Eeshita Dayani <eeshitadayani@gmail.com>
2023-12-12 21:56:36 -0500

    UI fix

--- src/components/IntroductionSection.tsx
+++ src/components/IntroductionSection.tsx
@@ -28,33 +28,35 @@ const IntroductionSection: React.FC = () => {
   }, []);
 
   return (
-    <div className={`${styles.introductionSection} ${styles.onScreen}`} id='intro' ref={sectionRef}>
-      <div className={styles.leftSection}>
-        <h2 className={styles.title}>About Us</h2>
-        <p className={styles.paragraph}>
-          Welcome to the UWaterloo Undergraduate Research Club, where
-          <span style={{ color: '#FFDB58' }}> curiosity meets opportunity.</span>
-        </p>
-      </div>
-      <Divider sx={{ backgroundColor: '#878787' }} orientation='vertical' flexItem />
-      <div className={styles.rightSection}>
-        <ul className={styles.list}>
-          <li className={styles.listItem}>
-            Engage in exciting research projects
-          </li>
-          <li className={styles.listItem}>
-            Connect with like-minded peers and mentors
-          </li>
-          <li className={styles.listItem}>
-            Explore your academic and professional interests
-          </li>
-          <li className={styles.listItem}>
-            Gain hands-on experience in your field of interest
-          </li>
-          <li className={styles.listItem}>
-            Collaborate with profs on meaningful projects
-          </li>
-        </ul>
+    <div id='intro' style={{paddingTop: '100px'}}>
+      <div className={`${styles.introductionSection} ${styles.onScreen}`} ref={sectionRef}>
+        <div className={styles.leftSection}>
+          <h2 className={styles.title}>About Us</h2>
+          <p className={styles.paragraph}>
+            Welcome to the UWaterloo Undergraduate Research Club, where
+            <span style={{ color: '#FFDB58' }}> curiosity meets opportunity.</span>
+          </p>
+        </div>
+        <Divider sx={{ backgroundColor: '#878787' }} orientation='vertical' flexItem />
+        <div className={styles.rightSection}>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>
+              Engage in exciting research projects
+            </li>
+            <li className={styles.listItem}>
+              Connect with like-minded peers and mentors
+            </li>
+            <li className={styles.listItem}>
+              Explore your academic and professional interests
+            </li>
+            <li className={styles.listItem}>
+              Gain hands-on experience in your field of interest
+            </li>
+            <li className={styles.listItem}>
+              Collaborate with profs on meaningful projects
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
